5c3c6755651f57f56c002d5906fef9430ed50935
hossein <hossein@hosseins-MacBook-Pro.local>
2024-10-07 10:25:59 +0330

    fix

--- src/app/kyc/page.js
+++ src/app/kyc/page.js
@@ -1,8 +1,13 @@
 "use client";
 // src/VideoRecorder.js
 import React, { useState, useRef, useMemo, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import CacheInput from "./cacheInput";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import CacheInput from "../cacheInput";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 import TextField from "@mui/material/TextField";
 
 import "./index.css";
@@ -15,7 +20,7 @@ import getEpoch from "./epoch";
 //   scope: "roles",
 // });
 
-// JSON.stringify({
+// JSON.stringify({§§
 //   client_id: "api-client-demo",
 //   client_secret: "21ba7936-ea0c-45ce-996d-887712f79799",
 //   grant_type: "client_credentials",
@@ -29,17 +34,44 @@ const Kyc = () => {
 
   const [token, setToken] = useState("");
   const [loading, setLoading] = useState(false);
+  const pathname = usePathname();
+  const env = pathname.split("/").pop();
+  // console.log(env);
+
+  useEffect(() => {
+    localStorage.setItem("env", env);
+  }, []);
+  //  const router = useRouter();
+  // const { uat } = router.query;
+  // const uat = searchParams; // Access the 'uat' query parameter
+
+  const sendMessageToApp2 = async (kycId, token) => {
+    try {
+      const res = await fetch("https://gateway-sdk.vercel.app/api/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ kycId, token }), // Send the message from App1
+      });
+
+      // const result = await res.json();
+      // setResponse(result); // Store the response from App2
+    } catch (error) {
+      console.error("Error sending message to App2:", error);
+    }
+  };
 
   const getToken = async (e) => {
     try {
-      const response = await fetch(`${baseUrl}/v1/auth/token`, {
+      const response = await fetch(`${baseUrl2}/v1/auth/token`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          client_id: "api-client-demo",
-          client_secret: "21ba7936-ea0c-45ce-996d-887712f79799",
+          client_id: "api-client-levant",
+          client_secret: "59c24382-18ac-41e5-9141-ef2dbcd2e8de",
           grant_type: "client_credentials",
           scope: "roles",
         }),
@@ -61,14 +93,15 @@ const Kyc = () => {
   useEffect(() => {
     getToken();
   }, []);
-  // {0,10} = 4,{10,20} = 4,{20,30} = 2,{30,40} = 2 {40,50}= 3 {50,60} = 3 {60,70} = 2 {70,80} = 3 {80,90 } = 2 {90,100} = 1 , {100,110} = 4 {110,120} = 1 {120,120} = 1 {130,140} = 3 {140,150} = 1 {160,170} = 2 {170,180} = 2 {180, 190} = 1 {190,200} = 4
-  // 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,51,53,59,61,67,71,73,79, 83,89 ,97,101,103,107,109,113 ,127, 131 ,137, 139 , 149 , 163,167 , 173 , 179 , 181, 191,193,197,199
+
   const [filterInputs, setFilterInput] = React.useState({
     nationalCode: "",
     birthYear: "",
     birthMonth: "",
     birthDay: "",
   });
+
+  const [objectDetection, setObjectDetection] = useState("HEAD_POSITIONING");
 
   const onChangeInput = (name) => (e) => {
     setFilterInput({ ...filterInputs, [name]: e.target.value });
@@ -82,7 +115,7 @@ const Kyc = () => {
     );
     try {
       const response = await fetch(
-        `${baseUrl}/v2/kyc/init/HEAD_POSITIONING/${filterInputs.nationalCode}/${birthDate}`,
+        `${baseUrl2}/v2/kyc/init/${objectDetection}/${filterInputs.nationalCode}/${birthDate}`,
         {
           method: "POST",
           headers: {
@@ -97,94 +130,49 @@ const Kyc = () => {
       // Check if the response is okay
       if (!response.ok) {
         const json = await response.json();
-        throw new Error("Network response was not ok");
+        alert(json.detail);
+        return json;
       }
       const json = await response.json();
 
-      const kycWindow = window.open("https://gateway-sdk.vercel.app", "_blank");
-      setTimeout(() => {
-        kycWindow.postMessage(
-          { token, kycId: json.kycId },
-          "https://gateway-sdk.vercel.app"
-        );
-      }, 1000);
-      console.log("sending message....");
+      localStorage.setItem("kycId", json.kycId);
+      // http://localhost:3000/
+      // https://kycgateway.mt.levants.io/
+      sendMessageToApp2(json.kycId, token);
+      const kycWindow = window.open(
+        "https://gateway-sdk.vercel.app/",
+        "_blank"
+      );
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          kycWindow.postMessage(
+            { token, kycId: json.kycId },
+            "https://gateway-sdk.vercel.app/"
+          );
+          resolve();
+        }, 1000);
+      });
 
-      // Wait for the new window to load, then send the data via postMessage
-      // kycWindow = () => {
-      //   console.log("sending message....");
-      //   kycWindow.postMessage(
-      //     { token, kycId: json.kycId },
-      //     "http://localhost:3000"
-      //   );
-      // };
-      // router.push(
-      //   `https://kyc-gateway.levants.io/${json.kycId}?callback=${window.location.host}`
-      // );
-
-      // router.push(
-      //   `https://kyc-gateway.mt.levants.io/${json.kycId}?callback=${window.location.origin}`
-      // );
-
-      // kyc-gateway.levants.io
-
-      // console.log(json);
-      // setOrderId(json.kycId);
-      // const actionsArray = json.action.split(",");
-      // Step 2: Create a dictionary for mapping
-      // const actionMappings = {
-      //   c: "center",
-      //   l: "left",
-      //   u: "up",
-      //   d: "down",
-      //   r: "right",
-      // };
-      // Step 3: Map array to corresponding meanings
-      // const mappedActions = actionsArray.map(
-      //   (action) => actionMappings[action]
-      // );
-      // const mapActionWithTitle = mappedActions.map((e) => {
-      //   if (e === "center") {
-      //     return {
-      //       title: "مرکز",
-      //       action: e,
-      //     };
-      //   } else if (e === "left") {
-      //     return {
-      //       title: "چپ",
-      //       action: e,
-      //     };
-      //   } else if (e === "right") {
-      //     return {
-      //       title: "راست",
-      //       action: e,
-      //     };
-      //   } else if (e === "down") {
-      //     return {
-      //       title: "پایین",
-      //       action: e,
-      //     };
-      //   } else if (e === "up") {
-      //     return {
-      //       title: "بالا",
-      //       action: e,
-      //     };
-      //   }
-      // });
-      // setLoading(false);
-      // setActions(mapActionWithTitle);
-      console.log(mapActionWithTitle);
+      if (
+        !kycWindow ||
+        kycWindow.closed ||
+        typeof kycWindow.closed == "undefined"
+      ) {
+        alert("Popup blocked! Please allow popups for this site.");
+      }
     } catch (error) {
+      console.log(error);
       setLoading(false);
+      alert(error);
       // theButton.classList.remove("buttonLoading");
       // theButton.classList.remove("disabled");
     }
   };
-
+  const handleChange = (event) => {
+    setObjectDetection(event.target.value);
+  };
   return (
-    <div
-      style={{ display: "flex", justifyContent: "center", background: "white" }}
-    >
+    <div style={{ display: "flex", justifyContent: "center" }}>
       <div className="main">
         <div
           style={{
@@ -254,6 +242,32 @@ const Kyc = () => {
                 dir="rtl"
               />
             </CacheInput>
+            <div>
+              <CacheInput>
+                <FormControl>
+                  <FormLabel id="demo-controlled-radio-buttons-group">
+                    type detection
+                  </FormLabel>
+                  <RadioGroup
+                    aria-labelledby="demo-controlled-radio-buttons-group"
+                    name="controlled-radio-buttons-group"
+                    value={objectDetection}
+                    onChange={handleChange}
+                  >
+                    <FormControlLabel
+                      value="HAND_GESTURE"
+                      control={<Radio />}
+                      label="HAND_GESTURE"
+                    />
+                    <FormControlLabel
+                      value="HEAD_POSITIONING"
+                      control={<Radio />}
+                      label="HEAD_POSITIONING"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </CacheInput>
+            </div>
           </div>
 
           <button
